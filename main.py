c358d4e37afbc7a8adda80e134c4ad5a745f9d04
Влад Черный <stalkervaultboy@gmail.com>
2023-10-31 19:13:46 +0400

    Added homework

---
+++ main.py
@@ -0,0 +1,46 @@
+import concurrent.futures
+import multiprocessing
+
+
+def worker(n):
+    core_name = multiprocessing.current_process().name
+    print(f'{core_name}: обработка данных => {n}')
+    print(f'{core_name}: обработка закончена => {n}')
+
+
+def summary(a):
+    s = []
+    final = 0
+    with concurrent.futures.ProcessPoolExecutor(max_workers=5) as executor:
+        wait_complete = []
+        # проходимся по списку с данными для обработки
+        for task in a:
+            row = 0
+            # запускаем worker() для каждого элемента данных
+            executor.submit(worker, task)
+            # в список складываем объекты 'future'
+            # с будущими результатами
+            # Здесь task - строка матрицы
+            wait_complete.append(task)
+            for i in task:
+                row += i
+            s.append(row)
+    for i in s:
+        final += i
+    print(final)
+
+
+def main():
+    print("Enter size of square matrix: \n")
+    a = int(input())
+    b = [[0] * a for _ in range(a)]
+    print(f"Enter {a} rows with {a} elems: \n")
+    for i in range(a):
+        for j in range(a):
+            b[i][j] = int(input())
+    print(b)
+    summary(b)
+
+
+if __name__ == '__main__':
+    main()
